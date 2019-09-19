f25e9e0d8524dacc25aeda76701d3b3f57d1ac6e
danicelistobon <danielct-97@hotmail.com>
2019-09-19 21:50:38 +0000

    modified task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        cp_list = []
-        for i in my_list:
-            if i is search:
-                cp_list.append(replace)
-            else:
-                cp_list.append(i)
-        return cp_list
+    cp_list = []
+    for i in my_list:
+        if i is search:
+            cp_list.append(replace)
+        else:
+            cp_list.append(i)
+    return cp_list
