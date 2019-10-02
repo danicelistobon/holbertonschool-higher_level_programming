654d56f6ccc9d6b2b596a7c6d2933edddc5d5b50
danicelistobon <danielct-97@hotmail.com>
2019-10-02 03:21:57 +0000

    created task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size):
+        """Initializes Square class
+
+        Args:
+            size (int): size of a square
+        """
+        self.__size = size
