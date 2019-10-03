cc5cfe14cce02f729e18b5a6aa526d584c1420b9
danicelistobon <danielct-97@hotmail.com>
2019-10-03 03:24:15 +0000

    modified2 task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,8 +6,8 @@ class Square:
     """Square class"""
     def __init__(self, size=0, position=(0, 0)):
         """Initializes Square class"""
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
