5118da13719951be97959b5add820c82b151145d
danicelistobon <danielct-97@hotmail.com>
2019-10-03 03:49:38 +0000

    created task 101

---
+++ 0x06-python-classes/101-square.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python3
+"""Script that defines a square"""
+
+
+class Square:
+    """Square class"""
+    def __init__(self, size=0, position=(0, 0)):
+        """Initializes Square class"""
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter to retrieve size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter to set size"""
+        if not isinstance(value, int):
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter to retrieve position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter to set position"""
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if not isinstance(value[0], int) or not isinstance(value[1], int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if (value[0] < 0) or (value[1] < 0):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Area of a square
+        Return:
+            returns the current square area
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints in stdout the square"""
+        if self.size == 0:
+            print()
+            return
+        for n in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for s in range(self.__position[0]):
+                print(" ", end="")
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    def __repr__(self):
+        if self.__size == 0:
+            return ("")
+        return (self.__position[1] * "\n" + (self.__size - 1) *
+                (self.__position[0] * " " + self.__size * "#" + "\n") +
+                self.__position[0] * " " + self.__size * "#")
