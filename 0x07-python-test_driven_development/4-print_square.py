d4143f08174db207d8ff17db0ea3b35668cfc777
danicelistobon <danielct-97@hotmail.com>
2019-10-05 02:27:14 +0000

    created task 3

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module to print a square with the character #
+    Args:
+        size (int): size length of the square
+
+"""
+
+
+def print_square(size):
+    """Function that prints a square with the character #
+        Return: printed square
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
