2a2c8d7a02d7fef4d0d2f3f9aab52ce69a910915
danicelistobon <danielct-97@hotmail.com>
2019-10-17 00:46:56 +0000

    From JSON string to Object

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module to return an object
+"""
+
+
+import json
+
+
+def from_json_string(my_str):
+    """Returns an object (Python data structure) represented by a JSON string
+    """
+
+    return json.loads(my_str)
