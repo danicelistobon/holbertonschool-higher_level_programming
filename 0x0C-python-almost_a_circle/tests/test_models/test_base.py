fab25ed0156d267afced7b919f8b66870ecd2ceb
danicelistobon <danielct-97@hotmail.com>
2019-10-23 04:46:39 +0000

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -33,3 +33,34 @@ class TestBase(unittest.TestCase):
         """
         base1 = Base()
         self.assertIsInstance(base1, object)
+
+    def test_docstring(self):
+        """Test docstring
+        """
+        self.assertIsNotNone(Base.__doc__)
+
+    def test_pep8(self):
+        """Test pep8
+        """
+        style = pep8.StyleGuide(quiet=True)
+        pep = style.check_files(["models/base.py"])
+        self.assertEqual(pep.total_errors, 0)
+
+    def test_to_json_string(self):
+        """Test to json string
+        """
+        r = Rectangle(1, 3, 4, 5, 7)
+        dic = r.to_dictionary()
+        json_dic = r.to_json_string(dic)
+        self.assertEqual(json_dic, json.dumps(dic))
+
+    def test_json_string_to_file(self):
+        """Test json string to file
+        """
+        r1 = Rectangle(2, 4, 6, 7, 3)
+        r2 = Rectangle(5, 4, 5, 6, 9)
+        Rectangle.save_to_file([r1, r2])
+        with open("Rectangle.json") as file:
+            new = file.read()
+            t2 = [r1.to_dictionary(), r2.to_dictionary()]
+            self.assertEqual(new, json.dumps(t2))
