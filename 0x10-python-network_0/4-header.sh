ac614352bc1ca489d248bffe256123355c041267
danicelistobon <danielct-97@hotmail.com>
2020-01-07 04:19:32 +0000

    4. cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL as an arg, sends a GET req and displays the body of the resp
-curl -sX GET "X-HolbertonSchool-User-Id:98" "$1"
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
