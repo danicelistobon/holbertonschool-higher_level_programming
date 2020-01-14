4115a7b7d3cf61ae5afaf47157904cdbc84e565a
danicelistobon <danielct-97@hotmail.com>
2020-01-14 04:38:58 +0000

    6. POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that in a URL and an email address, sends a POST request to the
+    passed URL with the email as a parameter, and finally displays the
+    body of the response
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    value = {'email': argv[2]}
+    response = requests.post(argv[1], data=value)
+    print(response.text)
