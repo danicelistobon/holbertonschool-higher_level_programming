872d59a099f93a0583a06861425cd9287dcc9500
danicelistobon <danielct-97@hotmail.com>
2020-02-05 01:32:28 +0000

    11. Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,22 @@
 #!/usr/bin/node
 const args = process.argv;
+const numbers = [];
+const without_big = [];
+let i;
+let biggest;
+let biggest2;
 if (args.length < 4) {
   console.log(0);
 } else {
-
+  for (i = 2; i < args.length; i++) {
+    numbers.push(parseInt(args[i]));
+  }
+  biggest = Math.max(...numbers);
+  for (i = 0; i < numbers.length; i++) {
+    if (numbers[i] !== biggest) {
+      without_big.push(parseInt(numbers[i]));
+    }
+  }
+  biggest2 = Math.max(...without_big);
+  console.log(biggest2);
 }
